c63e74f0ef4eb0bbd9c3c4ca80bf22d219795335
Zeeshan <rqmok@hotmail.com>
2014-04-19 22:56:52 +0930

    vivo: Initial Commit

---
+++ BoardConfig.mk
@@ -0,0 +1,48 @@
+USE_CAMERA_STUB := true
+
+LOCAL_PATH := device/htc/vivo
+
+# inherit from the proprietary version
+-include vendor/htc/vivo/BoardConfigVendor.mk
+
+TARGET_ARCH := arm
+TARGET_BOARD_PLATFORM := msm7x30
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := scorpion
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# Qualcomm
+BOARD_USES_QCOM_HARDWARE := true
+
+# Display
+BOARD_EGL_CFG := $(LOCAL_PATH)/egl.cfg
+
+# Partitions
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 585101312
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1232072704
+BOARD_BOOTIMAGE_PARTITION_SIZE := 5194304
+BOARD_FLASH_BLOCK_SIZE := 262144
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun0/file
+
+# Kernel
+# TARGET_PREBUILT_KERNEL := device/htc/vivo/kernel
+BOARD_KERNEL_CMDLINE := no_console_suspend=1
+BOARD_KERNEL_BASE := 0x04400000
+BOARD_KERNEL_PAGESIZE := 2048
+TARGET_KERNEL_CONFIG := vivo_defconfig
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := vivo
+TARGET_NO_BOOTLOADER := true
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/ramdisk/fstab.vivo
+
+# Headers
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+BOARD_HAS_NO_SELECT_BUTTON := true
---
+++ device_vivo.mk
@@ -0,0 +1,56 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/htc/vivo/vivo-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/htc/vivo/overlay
+
+LOCAL_PATH := device/htc/vivo
+
+# Ramdisk Files
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/ramdisk/fstab.sdcard:root/fstab.sdcard \
+	$(LOCAL_PATH)/ramdisk/fstab.vivo:root/fstab.vivo \
+	$(LOCAL_PATH)/ramdisk/init.htc7x30.usb.rc:root/init.htc7x30.usb.rc \
+	$(LOCAL_PATH)/ramdisk/init.vivo.rc:root/init.vivo.rc \
+	$(LOCAL_PATH)/ramdisk/ueventd.vivo.rc:root/ueventd.vivo.rc
+
+PERMISSION_PATH := frameworks/native/data/etc
+
+# Permission Files
+PRODUCT_COPY_FILES += \
+	$(PERMISSION_PATH)/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+	$(PERMISSION_PATH)/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+	$(PERMISSION_PATH)/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	$(PERMISSION_PATH)/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	$(PERMISSION_PATH)/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+	$(PERMISSION_PATH)/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+	$(PERMISSION_PATH)/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+	$(PERMISSION_PATH)/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	$(PERMISSION_PATH)/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+	$(PERMISSION_PATH)/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	$(PERMISSION_PATH)/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+	$(PERMISSION_PATH)/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml \
+	$(PERMISSION_PATH)/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+	$(PERMISSION_PATH)/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+# Media Files
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/audio_policy.conf:system/etc/audio_policy.conf \
+	$(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
+	$(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+	debug.composition.type=gpu \
+	debug.sf.hw=1 \
+	debug.egl.hw=1 \
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := full_vivo
+PRODUCT_DEVICE := vivo
